d8a5c5b4b106fa4f71c633062144d4d757ceb839
Drew Showalter <10665739+one19@users.noreply.github.com>
2022-10-24 18:11:49 +1100

    :recycle: split auth and context population.

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
-import getEnv from './env';
-
-import { Token, Context } from './types';
+import getEnv from './getEnv';
+import { GatewayEvent, Token } from './types';
 
 type DecodedToken = {
   iat: number;
@@ -11,40 +10,49 @@ type DecodedToken = {
 export const createToken = (data: Token, expiresIn: string = '35d') =>
   jwt.sign(data, getEnv().jwtSecret, { expiresIn });
 
-export const verifyToken = (token: string) => jwt.verify(token, getEnv().jwtSecret) as DecodedToken;
+export const verifyToken = (token: string = 'bad token') =>
+  jwt.verify(token, getEnv().jwtSecret) as DecodedToken;
 
 const twoWeeks = 20 * 24 * 60 * 60 * 1000;
 
 // sets the auth header so we can securely be logged in by the magic link
-export const setContext = async ({ event, context }: { event: any; context: Context }) => {
-  context.callbackWaitsForEmptyEventLoop = false;
+export default async (event: GatewayEvent) => {
+  const setCookies = [] as any[];
+  const setHeaders = [] as any[];
+  let email;
+  let type;
+  let id;
 
-  const { 'x-auth-token': token } = event.headers; // populate context with this
-  const { email, type, id, exp } = verifyToken(token);
+  try {
+    const { 'x-auth-token': token } = event.headers; // populate context with this
+    const { exp, ...rest } = verifyToken(token);
+    ({ email, type, id } = rest);
 
-  const setCookies = [];
+    if (exp * 1000 - Date.now() < twoWeeks) {
+      // set a cookie if the token is near the expiry
 
-  if (exp * 1000 - Date.now() < twoWeeks) {
-    // set a cookie if the token is near the expiry
-
-    const validToken = createToken({ email, type, id });
-    setCookies.push({
-      name: 'auth cookie',
-      value: validToken,
-      options: {
-        expires: new Date(Date.now() + twoWeeks),
-        domain: 'boarganise.com',
-        maxAge: twoWeeks / 1000,
-        httpOnly: true,
-        sameSite: true,
-        secure: true,
-        path: '/',
-      },
-    });
+      const validToken = createToken({ email, type, id });
+      setCookies.push({
+        name: 'auth cookie',
+        value: validToken,
+        options: {
+          expires: new Date(Date.now() + twoWeeks),
+          domain: 'boarganise.com',
+          maxAge: twoWeeks / 1000,
+          httpOnly: true,
+          sameSite: true,
+          secure: true,
+          path: '/',
+        },
+      });
+    }
+  } catch (err) {
+    console.error('this was a mistake', err);
   }
 
   return {
     setCookies,
+    setHeaders,
     event,
     email,
     type,
---
+++ src/setContext.ts
@@ -0,0 +1,29 @@
+import { model } from 'dynamoose';
+import type { BaseContext, ContextFunction } from '@apollo/server';
+import type { LambdaContextFunctionArgument, MagicUserItem, Context } from './types';
+import { MagicUserSchema } from './schemas';
+import getAuth from './auth';
+import getEnv from './getEnv';
+
+// sets the auth header so we can securely be logged in by the magic link
+const setContext: ContextFunction<[LambdaContextFunctionArgument], BaseContext> = async ({
+  event,
+  context,
+}): Promise<Context> => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const { setCookies, setHeaders, email, type, id } = await getAuth(event);
+  const MagicUser = model<MagicUserItem>(getEnv().tableName, MagicUserSchema);
+
+  return {
+    MagicUser,
+    setCookies,
+    setHeaders,
+    event,
+    email,
+    type,
+    id,
+  };
+};
+
+export default setContext;
