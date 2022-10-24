802df2e628b7c87b019ab7389ab08e40174d985a
Drew Showalter <10665739+one19@users.noreply.github.com>
2022-10-24 20:48:08 +1100

    :fire: clean up the DB on dev destroy.

--- stacks/Database.ts
+++ stacks/Database.ts
@@ -1,18 +1,24 @@
 import { Config, StackContext, Table } from '@serverless-stack/resources';
+import { RemovalPolicy } from 'aws-cdk-lib';
 
 export function Database({ stack }: StackContext) {
-  const table = new Table(stack, 'table', {
+  const table = new Table(stack, 'users-table', {
     fields: {
       id: 'string',
     },
     primaryIndex: {
       partitionKey: 'id',
     },
+    cdk: {
+      table: {
+        removalPolicy: stack.stage === 'dev' ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
+      },
+    },
   });
 
   return {
     table,
-    TABLE_NAME: new Config.Parameter(stack, 'users', {
+    TABLE_NAME: new Config.Parameter(stack, 'USERS_TABLE_NAME', {
       value: table.tableName,
     }),
   };
