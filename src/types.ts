82608ee15fa80abfe19d3cc1892e2514459418c9
Drew Showalter <10665739+one19@users.noreply.github.com>
2022-10-24 18:16:36 +1100

    :memo: proper ts typing for context everything.

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,42 @@
-import { Table } from 'dynamoose';
+// import type { Entity } from 'dynamodb-toolbox';
+import type { Model } from 'dynamoose/dist/Model';
+import type { Item } from 'dynamoose/dist/Item';
+import type {
+  Context as LambdaContext,
+  APIGatewayProxyEventV2,
+  APIGatewayProxyEvent,
+} from 'aws-lambda';
+
+export type GatewayEvent = APIGatewayProxyEvent | APIGatewayProxyEventV2;
+export interface LambdaContextFunctionArgument {
+  event: GatewayEvent;
+  context: LambdaContext;
+}
 
 export type UserType = {
-  secretToken: string | null;
+  secretToken?: string | null;
   createdAt: string;
   updatedAt: string;
   settings: any;
   email: string;
-  name: string;
+  name?: string;
   type: string; // magic | google | etc.
   id: string;
 };
+export type MagicUserItem = Item & UserType;
 
 export type IdObject = {
   id: string;
 };
 
 export type Context = {
-  dataSources: {
-    userSource: typeof Table;
-  };
-  callbackWaitsForEmptyEventLoop?: boolean;
-  email: string;
-  type: string;
-  id: string;
+  MagicUser: Model<MagicUserItem>;
+  setCookies: string[];
+  setHeaders: string[];
+  email?: string;
+  type?: string;
+  id?: string;
+  event: any;
 };
 
 export type Token = {
