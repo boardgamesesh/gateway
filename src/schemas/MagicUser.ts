22ee56fdd2550b92c4afc77998f20ad016e672d1
Drew Showalter <10665739+one19@users.noreply.github.com>
2022-11-04 21:19:46 +1100

    :sparkles: add sessionIds for batchGet.

--- src/schemas/MagicUser.ts
+++ src/schemas/MagicUser.ts
@@ -8,6 +8,7 @@ export default new Schema(
     location: { type: String },
     secretToken: { type: [String, type.NULL] },
     friendIds: { type: Array, schema: [String] },
+    sessionIds: { type: Array, schema: [String] },
     groupHashIds: { type: Array, schema: [String] },
     settings: { type: Object, schema: { darkMode: Boolean } },
     email: { type: String, required: true, index: { name: 'email', type: 'global' } },
